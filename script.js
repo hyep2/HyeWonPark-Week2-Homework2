01cb7d8ea0d7d6132d05956fb8aa2e8e425c5ef6
Hye Won Park <hyewonpark26@gmail.com>
2022-02-24 00:30:27 -0800

    added necessary links

---
+++ script.js
@@ -0,0 +1,20 @@
+//creating object of questions,options,and answers
+let questionSet = [
+  {
+    question: 'Which data type stores TRUE or FALSE values?';
+    options: ['array', 'number', 'boolean'];
+    answer: 'boolean';
+  }
+
+      {
+    question: 'String values must be enclosed in ______?';
+    options: ['brackets', 'quotes', 'semicolon'];
+    answer: 'quotes';
+  }
+
+      {
+    question: 'Which data type stores TRUE or FALSE values?';
+    options: ['array', 'number', 'boolean'];
+    answer: 'boolean';
+  }
+]
