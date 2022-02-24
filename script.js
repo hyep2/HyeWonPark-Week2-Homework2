002003531a7c34b3932c9ef9bf1580beb806d993
Hye Won Park <hyewonpark26@gmail.com>
2022-02-24 03:48:07 -0800

    code

--- script.js
+++ script.js
@@ -15,55 +15,71 @@ let questionSet = [
     option: ['let', 'var', 'const'],
     answer: 'var'
   }
+  // },
+  // {
+  //   question: '4. String values must be enclosed in ______?',
+  //   option: ['let', 'var', 'const'],
+  //   answer: 'var'
+  // }
 ]
 
 
-//if start button pressed, then container (containing the question/options) appear
+//if start button pressed, then container (containing the question/options) appear and triggers the entire runthrough of the game
 document.getElementById('start').addEventListener('click', event => {
   event.preventDefault();
   document.querySelector('.container').style.visibility = 'visible';
   takeQuiz();
+  gameOver();
 })
 
 
 let questionNumber = 0;
+let scoreCount = 0;
 function takeQuiz() {
   //will display first question
   document.getElementById('question').textContent = questionSet[questionNumber].question;
+  
   //will display answerchoices
   document.getElementById('myButton1').value = questionSet[questionNumber].option[0]
   document.getElementById('myButton2').value = questionSet[questionNumber].option[1]
   document.getElementById('myButton3').value = questionSet[questionNumber].option[2]
 
 
-  //checking for each answer choice 
+  //checking for each answer choice ; if correct, score increases by 10 ; if wrong answer, deducts -2 points
   document.getElementById('myButton1').addEventListener('click', event => {
     if (questionSet[questionNumber].option[0] === questionSet[questionNumber].answer) {
+      scoreCount += 10;
       nextQuestion();
     }
     else {
+      scoreCount -= 2;
       document.getElementById('verifyAnswer').innerHTML = "WRONG, TRY AGAIN!"
     }
 
   })
   document.getElementById('myButton2').addEventListener('click', event => {
     if (questionSet[questionNumber].option[1] === questionSet[questionNumber].answer) {
+      scoreCount += 10;
       nextQuestion();
     }
     else {
+      scoreCount -= 2;
       document.getElementById('verifyAnswer').innerHTML = "WRONG, TRY AGAIN!"
     }
 
   })
   document.getElementById('myButton3').addEventListener('click', event => {
     if (questionSet[questionNumber].option[2] === questionSet[questionNumber].answer) {
+      scoreCount += 10;
       nextQuestion();
     }
     else {
+      scoreCount -= 2;
       document.getElementById('verifyAnswer').innerHTML = "WRONG, TRY AGAIN!"
     }
-
+  console.log(scoreCount)
   })
+  
 }
 
 //will move onto next question if user gets it right
@@ -72,12 +88,19 @@ function nextQuestion() {
   document.getElementById('verifyAnswer').innerHTML = "";
   questionNumber = questionNumber + 1;
   //if it is at the last question, will display final score
-  if (questionNumber > 2) {
-    return; //need to call fxn that will go to game over page
+  if (questionNumber==3) {
+    return;
   }
-  takeQuiz();
+  else {
+    takeQuiz();
+  }
+  
 }
 
-function gameOver() {
 
+//when game is over, will display total score and a form where the user can save initials and score
+function gameOver() {
+  return;
+  // document.querySelector('.gameOver').visibility = 'visible';
+  // document.getElementById('score').innerHTML = scoreCount;
 }
